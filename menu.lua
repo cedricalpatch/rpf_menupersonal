577c97741857909e4eccb9fbd9a549f43704be14
RPF Studio <groupe-esp-fr@outlook.fr>
2019-12-23 21:07:36 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -119,3 +119,11 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
     end
 end)
+
+function whenKeyJustPressed(key)
+    if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
+        return true
+    else
+        return false
+    end
+end
