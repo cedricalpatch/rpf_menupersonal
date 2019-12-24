c31d69c51e35161df059180e089a40fcb5c04aec
RPF Studio <groupe-esp-fr@outlook.fr>
2019-12-24 12:22:52 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -2,11 +2,11 @@
 
 Citizen.CreateThread(function()
     local checkbox2 = false
-    WarMenu.CreateMenu('perso', "Menu Perso")
+    WarMenu.CreateMenu('perso', "Personel")
     WarMenu.SetSubTitle('perso', 'Information joueur')
-    WarMenu.CreateSubMenu('inv', 'perso', 'Votre inventaire')
-    WarMenu.CreateSubMenu('inv1', 'perso', 'Tenue disponible')
-    WarMenu.CreateSubMenu('inv2', 'perso', 'Arme disponible')
+    WarMenu.CreateSubMenu('inv', 'perso', 'Your inventory')
+    WarMenu.CreateSubMenu('inv1', 'perso', 'Your clothing')
+    WarMenu.CreateSubMenu('inv2', 'perso', 'Your Weapon')
 
 
     while true do
@@ -16,98 +16,45 @@ Citizen.CreateThread(function()
 
         if WarMenu.IsMenuOpened('perso') then
 
-                HasStreamedTextureDictLoaded("menu_textures")
-                DrawSprite("menu_textures", "translate_bg_1a", 0.55, 0.2, 0.35, 0.70, 0.8, 000, 2, 2, 200, 1)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.5, 0.5)
-                DisplayText("Invetaire", 0.41, 0.06)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("Vous avez un cheval et votre garde du corp !", 0.41, 0.11)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("N'abuzez pas des bug ne spamer pas la touche ...", 0.41, 0.13)
-                TriggerServerEvent("redemrp_inventory:itemsList") 
-
             if WarMenu.MenuButton('Inventaire', 'inv') then 
 
-                HasStreamedTextureDictLoaded("menu_textures")
-                DrawSprite("menu_textures", "translate_bg_1a", 0.55, 0.2, 0.35, 0.70, 0.8, 000, 2, 2, 200, 1)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.5, 0.5)
-                DisplayText("Invetaire", 0.41, 0.06)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("Vous avez un cheval et votre garde du corp !", 0.41, 0.11)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("N'abuzez pas des bug ne spamer pas la touche ...", 0.41, 0.13)
             end
 
             if WarMenu.MenuButton('Tenues', 'inv1') then
 
-                HasStreamedTextureDictLoaded("menu_textures")
-                DrawSprite("menu_textures", "translate_bg_1a", 0.55, 0.2, 0.35, 0.70, 0.8, 000, 2, 2, 200, 1)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.5, 0.5)
-                DisplayText("Invetaire", 0.41, 0.06)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("Vous avez un cheval et votre garde du corp !", 0.41, 0.11)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("N'abuzez pas des bug ne spamer pas la touche ...", 0.41, 0.13)
             end
 
-            if WarMenu.MenuButton('Armes', 'inv2') then
-                                HasStreamedTextureDictLoaded("menu_textures")
-                DrawSprite("menu_textures", "translate_bg_1a", 0.55, 0.2, 0.35, 0.70, 0.8, 000, 2, 2, 200, 1)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.5, 0.5)
-                DisplayText("Invetaire", 0.41, 0.06)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("Vous avez un cheval et votre garde du corp !", 0.41, 0.11)
-                SetTextColor(255, 255, 255, 255)
-                SetTextFontForCurrentCommand(1)
-                SetTextScale(0.3, 0.3)
-                DisplayText("N'abuzez pas des bug ne spamer pas la touche ...", 0.41, 0.13)
+            if WarMenu.MenuButton('Weapon', 'inv2') then
+
             end
 
             WarMenu.Display()
         elseif WarMenu.IsMenuOpened('inv1') then
-            if WarMenu.Button('Perso de base') then
+            if WarMenu.Button('Clothe saved') then
                 TriggerServerEvent("rpf_clotheshop:loadClothes")
-            elseif WarMenu.Button('Perso modifier') then
-                TriggerServerEvent("redem_clothing:loadClothes")
-            elseif WarMenu.Button('Perso Online') then
+                --Citizen.InvokeNative(0xA5BAE410B03E7371, PlayerPedId(), 46, 0, 0, 0)
+
+            elseif WarMenu.Button('Outfit saved') then
+                    TriggerServerEvent("redemrp_clothing:loadClothes", 1, function(cb)
+                    end)
+            elseif WarMenu.Button('Skin saved') then
                  TriggerServerEvent("redemrp_skin:loadSkin")
             end
 
             WarMenu.Display()
         elseif WarMenu.IsMenuOpened('inv') then   
 
-            if WarMenu.Button('Votre cheval') then
+            if WarMenu.Button('Your Horse') then
                 TriggerServerEvent('elrp:loadhorse')
+                --TriggerServerEvent("redemrp_inventory:itemsList", 1)
             end
             WarMenu.Display()
 
         elseif WarMenu.IsMenuOpened('inv2') then   
 
-            if WarMenu.Button('Revelver') then
+            if WarMenu.Button('Revolver') then
                 Citizen.InvokeNative(0xB282DC6EBD803C75, GetPlayerPed(), GetHashKey("WEAPON_REVOLVER_CATTLEMAN"), 500, true, 0)
-            elseif WarMenu.Button('Carabine') then
+            elseif WarMenu.Button('Rifle') then
                 Citizen.InvokeNative(0xB282DC6EBD803C75, GetPlayerPed(), GetHashKey("WEAPON_SNIPERRIFLE_ROLLINGBLOCK"), 500, true, 0)
             end
 
@@ -119,11 +66,3 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
     end
 end)
-
-function whenKeyJustPressed(key)
-    if Citizen.InvokeNative(0x580417101DDB492F, 0, key) then
-        return true
-    else
-        return false
-    end
-end
